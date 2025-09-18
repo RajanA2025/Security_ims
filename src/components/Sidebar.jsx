b76aa5556b1584a216713fd06185f70c95f89183
roshanjitglobal <roshan.s@jitglobalinfosystems.com>
2025-09-18 17:37:08 +0530

    test 2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = ({ isExpanded, setIsExpanded }) => {
   const [openMenu, setOpenMenu] = useState(null); // track which menu is open
 
   const toggleSidebar = () => setIsExpanded((prev) => !prev);
-  var layout ="1"
+  var layout ="2"
 
   const navItems = [
     { icon: <MdDashboard size={30}/>, label: "Dashboard", path: "/Security" },
--- src/pages/operational/Observability/Observability.jsx
+++ src/pages/operational/Observability/Observability.jsx
@@ -31,14 +31,20 @@ const Observability = () => {
   const [loading, setLoading] = useState(false);
   const [securityData, setSecurityData] = useState([]);
   const [eipData, seteipData] = useState([]);
-  const [volumeData, setvolumeData] = useState([]);
+  const [volumeData, setVolumeData] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [isModalOpen1, setIsModalOpen1] = useState(false);
   const [selectedData, setSelectedData] = useState(null);
   const [selectedData1, setSelectedData1] = useState(null);
+  const [s3Data, setS3Data] = useState([]);
+  const [ec2Data, setEC2Data] = useState([]);
 
   const [searchText, setSearchText] = useState("");
+  //test for search name id
+  const [searchName, setSearchName] = useState("");
+  const [searchId, setSearchId] = useState("");
+
 
   // Fetch data
   useEffect(() => {
@@ -46,16 +52,24 @@ const Observability = () => {
       setLoading(true);
       try {
         if (tabKey === "1") {
-          const res = await axios.get("http://13.212.15.14:8012/keypairs2");
+          const res = await axios.get("http://13.212.15.14:8016/keypairs2");
           setSecurityData(res.data);
         } else if (tabKey === "2") {
-          const res = await axios.get("http://13.212.15.14:8012/orphaned_eip");
+          const res = await axios.get("http://13.212.15.14:8016/orphaned-eip");
           seteipData(res.data);
         }
         else if (tabKey === "3") {
-            const res = await axios.get("http://13.212.15.14:8012/orphaned_volumes");
-            setvolumeData(res.data);
-          }
+          const res = await axios.get("http://13.212.15.14:8016/orphaned-volumes");
+          setVolumeData(res.data);
+        }
+        if (tabKey === "4") {
+          const res = await axios.get("http://13.212.15.14:8016/s3");
+          setS3Data(res.data);
+        } else if (tabKey === "5") {
+          const res = await axios.get("http://13.212.15.14:8016/ec2");
+          setEC2Data(res.data);
+        }
+
       } catch (err) {
         console.error("Error fetching data:", err);
       } finally {
@@ -110,20 +124,7 @@ const Observability = () => {
       filters: getUniqueOptions(securityData, "region"),
       onFilter: (value, record) => record.region === value
     },
-    {
-      title: (
-        <span>
-          Tool{" "}
-          <Tooltip title="Security tool used in AWS.">
-            <InfoCircleOutlined style={{ color: "#1890ff", cursor: "pointer" }} />
-          </Tooltip>
-        </span>
-      ),
-      dataIndex: "tool",
-      key: "tool",
-      filters: getUniqueOptions(securityData, "tool"),
-      onFilter: (value, record) => record.tool === value
-    },
+
     {
       title: (
         <span>
@@ -158,21 +159,114 @@ const Observability = () => {
     }
   ];
 
+  // EIP Tab Columns
+  const eipColumns = [
+    {
+      title: 'Account ID',
+      dataIndex: 'account_id',
+      key: 'account_id',
+    },
+    {
+      title: 'Account Name',
+      dataIndex: 'account_name',
+      key: 'account_name',
+    },
+    {
+      title: 'Region',
+      dataIndex: 'region',
+      key: 'region',
+    },
+    {
+      title: 'Allocation ID',
+      dataIndex: 'allocation_id',
+      key: 'allocation_id',
+    },
+    {
+      title: 'arn',
+      dataIndex: 'arn',
+      key: 'arn',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      render: status => (
+        <Tag color={status === 'in-use' ? 'green' : 'red'}>{status}</Tag>
+      )
+    },
+    {
+      title: "More Details",
+      key: "action",
+      render: (_, record) => (
+        <Tooltip title="View Details">
+          <EyeOutlined
+            style={{ fontSize: 18, color: "#1890ff", cursor: "pointer" }}
+            onClick={() => handleOpenModal(record)}
+          />
+        </Tooltip>
+      )
+    }
+  ];
+
+  // Volume Tab Columns
+  const volumeColumns = [
+    {
+      title: 'Account ID',
+      dataIndex: 'account_id',
+      key: 'account_id',
+    },
+    {
+      title: 'Account Name',
+      dataIndex: 'account_name',
+      key: 'account_id',
+    },
+    {
+      title: 'Region',
+      dataIndex: 'region',
+      key: 'region',
+    },
+    {
+      title: 'Volume ID',
+      dataIndex: 'volume_id',
+      key: 'volume_id',
+    },
+    {
+      title: 'Size (GB)',
+      dataIndex: 'size',
+      key: 'size',
+    },
+    {
+      title: "More Details",
+      key: "action",
+      render: (_, record) => (
+        <Tooltip title="View Details">
+          <EyeOutlined
+            style={{ fontSize: 18, color: "#1890ff", cursor: "pointer" }}
+            onClick={() => handleOpenModal(record)}
+          />
+        </Tooltip>
+      )
+    }
+
+  ];
+
+
+
   // KMS Tab Columns
-  const columns1 = [
+  const kmsColumns = [
     {
       title: (
         <span>
           Account Id{" "}
-          <Tooltip title="The AWS account's name.">
+          <Tooltip title="The AWS account's ID.">
             <InfoCircleOutlined style={{ color: "#1890ff", cursor: "pointer" }} />
           </Tooltip>
         </span>
       ),
-      dataIndex: "aws_account",
-      key: "aws_account",
-      filters: getUniqueOptions(eipData, "aws_account"),
-      onFilter: (value, record) => record.aws_account === value
+      dataIndex: "account_id",
+      key: "account_id",
+      filters: getUniqueOptions(eipData, "account_id"),
+      onFilter: (value, record) => record.account_id === value
     },
     {
       title: (
@@ -205,7 +299,7 @@ const Observability = () => {
         <Tag color={value ? "green" : "red"}>{value ? "Enabled" : "Disabled"}</Tag>
       )
     },
-    
+
     {
       title: (
         <span>
@@ -217,7 +311,7 @@ const Observability = () => {
       ),
       dataIndex: "last_accessed_service",
       key: "last_accessed_service",
-    
+
     },
     {
       title: (
@@ -263,7 +357,7 @@ const Observability = () => {
       title: "LastUsed Date",
       dataIndex: "last_used_date",
       key: "last_used_date",
-    
+
     },
     // {
     //   title: "Creation Date",
@@ -283,41 +377,148 @@ const Observability = () => {
       )
     }
   ];
+  const s3Columns = [
+    {
+      title: "Account ID", dataIndex: "account_id", key: "account_id"
+    },
+    { title: "Account Name", dataIndex: "account_name", key: "account_name" },
+    { title: "Bucket Name", dataIndex: "bucket_name", key: "bucket_name" },
+    {
+      title: "Versioning",
+      dataIndex: "versioning_status",
+      key: "versioning_status",
+      render: value => {
+        if (!value) {
+          // Bucket never had versioning enabled
+          return <Tag color="default">Unversioned</Tag>;
+        }
+        if (value.toLowerCase() === "enabled") {
+          return <Tag color="green">Enabled</Tag>;
+        }
+        if (value.toLowerCase() === "suspended") {
+          return <Tag color="orange">Suspended</Tag>;
+        }
+        return <Tag color="default">{value}</Tag>; // fallback for unexpected values
+      }
+    },
+    {
+      title: "Public Access Block",
+      dataIndex: "public_access_block",
+      key: "public_access_block",
+      render: value => (
+        <Tag color={value ? "green" : "red"}>
+          {value ? "Enabled" : "Disabled"}
+        </Tag>
+      )
+    },
+
+    { title: "Region", dataIndex: "region", key: "region" },
+    {
+      title: "replication_status",
+      dataIndex: "replication_status",
+      key: "replication_status",
+      render: value => (
+        <Tag color={value ? "green" : "red"}>
+          {value ? "Enabled" : "Disabled"}
+        </Tag>
+      )
+    },
+    {
+      title: "MFA Delete",
+      dataIndex: "mfa_delete",
+      key: "mfa_delete",
+      render: value => (value ? "✅" : "❌")
+    },
+
+    { title: "object_count", dataIndex: "object_count", key: "object_count" },
+    { title: "bucket_size_gb", dataIndex: "bucket_size_gb", key: "bucket_size_gb" },
+    {
+      title: "More Details",
+      key: "action",
+      render: (_, record) => (
+        <Tooltip title="View Details">
+          <EyeOutlined
+            style={{ fontSize: 18, color: "#1890ff", cursor: "pointer" }}
+            onClick={() => handleOpenModal(record)}
+          />
+        </Tooltip>
+      )
+    }
+  ];
+  const ec2Columns = [
+    { title: "Account ID", dataIndex: "account_id", key: "account_id" },
+    { title: "Account Name", dataIndex: "account_name", key: "account_name" },
+    { title: "Instance ID", dataIndex: "instance_id", key: "instance_id" },
+    { title: "Type", dataIndex: "instance_type", key: "instance_type" },
+    {
+      title: "State",
+      dataIndex: "state",
+      key: "state",
+      render: value => (
+        <Tag color={value === "running" ? "green" : "red"}>{value}</Tag>
+      )
+    },
+    
+    {
+      title: "More Details",
+      key: "action",
+      render: (_, record) => (
+        <Tooltip title="View Details">
+          <EyeOutlined
+            style={{ fontSize: 18, color: "#1890ff", cursor: "pointer" }}
+            onClick={() => handleOpenModal(record)}
+          />
+        </Tooltip>
+      )
+    }
+  ];
+
+
 
   return (
     <>
-     
+
 
       <Row gutter={[16, 16]} style={{ justifyContent: "flex-end" }}>
         <Col md={20}>
-        <Typography.Title 
-  level={4}
-  style={{
-    fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
-    fontSize: "20px",
-    fontWeight: 500,
-    color: "black",
-    margin: 0
-  }}
->
-Observability
-</Typography.Title>
-      
-     
+          <Typography.Title
+            level={4}
+            style={{
+              fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+              fontSize: "20px",
+              fontWeight: 500,
+              color: "black",
+              margin: 0
+            }}
+          >
+            Observability
+          </Typography.Title>
+
+
         </Col>
         <Col md={4}>
           <Input
             placeholder="Search by Account Name"
             prefix={<SearchOutlined />}
-            value={searchText}
-            onChange={handleSearch}
+            value={searchName}
+            onChange={e => setSearchName(e.target.value)}
             allowClear
           />
         </Col>
+        <Col md={4}>
+          <Input
+            placeholder="Search by Account ID"
+            prefix={<SearchOutlined />}
+            value={searchId}
+            onChange={e => setSearchId(e.target.value)}
+            allowClear
+          />
+        </Col>
+
       </Row>
 
-      <Tabs 
-        defaultActiveKey="1" 
+      <Tabs
+        defaultActiveKey="1"
         onChange={key => setTabKey(key)}
         style={{
           marginTop: "0px",
@@ -328,35 +529,68 @@ Observability
           <Table
             columns={columns}
             dataSource={securityData.filter(item =>
-              item.account_name?.toLowerCase().includes(searchText.toLowerCase())
+              (item.account_name?.toLowerCase().includes(searchName.toLowerCase())) &&
+              (item.account_id?.toLowerCase().includes(searchId.toLowerCase()))
             )}
+
             loading={loading}
-            rowKey="username"
+            rowKey={(record) => record.allocation_id || record.volume_id || record.key_id || 'key'}
             pagination={{ pageSize: 8 }}
           />
         </Tabs.TabPane>
         <Tabs.TabPane tab="Unassociated Elastic IP" key="2">
           <Table
-            columns={columns1}
-            dataSource={eipData.filter(item =>
-              item.aws_account?.toLowerCase().includes(searchText.toLowerCase())
-            )}
+            columns={eipColumns}
+            dataSource={securityData.filter(item =>
+  (item.account_name?.toLowerCase().includes(searchName.toLowerCase())) &&
+  (item.account_id?.toLowerCase().includes(searchId.toLowerCase()))
+)}
+
             loading={loading}
-            rowKey="username"
+            rowKey={(record) => record.allocation_id || 'eip-key'}
             pagination={{ pageSize: 8 }}
           />
         </Tabs.TabPane>
         <Tabs.TabPane tab="Orphaned volume" key="3">
           <Table
-            columns={columns1}
-            dataSource={eipData.filter(item =>
-              item.aws_account?.toLowerCase().includes(searchText.toLowerCase())
+            columns={volumeColumns}
+            dataSource={volumeData.filter(item =>
+              (item.aws_account?.toLowerCase() || '').includes(searchText.toLowerCase()) ||
+              (item.volume_id?.toLowerCase() || '').includes(searchText.toLowerCase())
             )}
             loading={loading}
-            rowKey="username"
+            rowKey={(record) => record.volume_id || 'volume-key'}
             pagination={{ pageSize: 8 }}
           />
         </Tabs.TabPane>
+        <Tabs.TabPane tab="S3 Details" key="4">
+  <Table
+    columns={s3Columns}
+    dataSource={s3Data.filter(item =>
+      (item.account_name?.toLowerCase().includes(searchName.toLowerCase())) &&
+      (item.account_id?.toLowerCase().includes(searchId.toLowerCase()))
+    )}
+    loading={loading}
+    rowKey={record => record.bucket_name || 's3-key'}
+    pagination={{ pageSize: 8 }}
+  />
+</Tabs.TabPane>
+
+
+        <Tabs.TabPane tab="EC2 Details" key="5">
+  <Table
+    columns={ec2Columns}
+    dataSource={ec2Data.filter(item =>
+      (item.account_name?.toLowerCase().includes(searchName.toLowerCase())) &&
+      (item.account_id?.toLowerCase().includes(searchId.toLowerCase()))
+    )}
+    loading={loading}
+    rowKey={record => record.instance_id || 'ec2-key'}
+    pagination={{ pageSize: 8 }}
+  />
+</Tabs.TabPane>
+
+
       </Tabs>
 
       <Modal
@@ -388,7 +622,7 @@ Observability
         {selectedData1 && (
           <Card size="small" title="Information" style={{ marginBottom: 16 }} headStyle={header}>
             <Descriptions bordered column={2} size="small">
-              <Descriptions.Item label="Account ID">{selectedData1.aws_account}</Descriptions.Item>
+              <Descriptions.Item label="Account ID">{selectedData1.account_id}</Descriptions.Item>
               <Descriptions.Item label="Key Id">{selectedData1.key_id}</Descriptions.Item>
               <Descriptions.Item label="key ARN">{selectedData1.key_arn}</Descriptions.Item>
               <Descriptions.Item label="Origin">{selectedData1.origin}</Descriptions.Item>
